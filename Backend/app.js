8a94691015c802af46feec210b334f8057a86635
tora0001 <tobe.2309@hotmail.com>
2023-09-19 10:53:30 +0200

    backend

---
+++ Backend/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+import "dotenv/config";
+import fs from "fs";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Testdata APP listening on port ${port}`));
+
+const dbconfig = {
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+};
+
+if (process.env.MYSQL_CERT) {
+  dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
+}
+
+const connection = mysql.createConnection(dbconfig);
+
+app.get("/musicbase", (req, res) => {
+  connection.query(`SELECT * FROM albums`, function (err, results, fields) {
+    console.log("ERR:");
+    console.log(err);
+    console.log("RESULTS:");
+    console.log(results);
+    console.log("FIELDS:");
+    console.log(fields);
+
+    res.json(results);
+  });
+});
