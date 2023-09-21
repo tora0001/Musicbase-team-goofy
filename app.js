1bf10fb6d8a33aedf63a62dfa553aa274feacd55
frva0001 <frva0001@gmail.com>
2023-09-21 22:23:33 +0200

    delete function artists added

--- app.js
+++ app.js
@@ -12,48 +12,78 @@ const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Testdata APP listening on port ${port}`));
 
+// establish database connection
+
 const dbconfig = {
-  host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DATABASE,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+   host: process.env.MYSQL_HOST,
+   database: process.env.MYSQL_DATABASE,
+   user: process.env.MYSQL_USER,
+   password: process.env.MYSQL_PASSWORD,
 };
 
 if (process.env.MYSQL_CERT) {
-  dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
+   dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
 }
 
 const connection = mysql.createConnection(dbconfig);
 
+// artists CRUD functions
+
+// read / get
+
 app.get("/artists", (request, response) => {
-  const query = "SELECT * FROM artists ORDER BY name";
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    } else {
-      response.json(results);
-    }
-  });
+   const query = "SELECT * FROM artists ORDER BY name";
+   connection.query(query, (error, results, fields) => {
+      if (error) {
+         console.log(error);
+      } else {
+         response.json(results);
+      }
+   });
 });
+
+// create / post
+
+// update / put
+
+// delete / delete
+
+app.delete("/artists/:id", (request, response) => {
+   const id = request.params.id;
+   const query = "DELETE FROM artists WHERE id=?;";
+   const values = [id];
+
+   connection.query(query, values, (error, results, fields) => {
+      if (error) {
+         console.log(error);
+      } else {
+         response.json(results);
+      }
+   });
+});
+
+// albums CRUD functions
 
 app.get("/albums", (request, response) => {
-  const query = "SELECT * FROM albums  ORDER BY albumName";
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    } else {
-      response.json(results);
-    }
-  });
+   const query = "SELECT * FROM albums  ORDER BY albumName";
+   connection.query(query, (error, results, fields) => {
+      if (error) {
+         console.log(error);
+      } else {
+         response.json(results);
+      }
+   });
 });
 
+// songs CRUD functions
+
 app.get("/songs", (request, response) => {
-  const query = "SELECT * FROM songs ORDER BY songName";
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    } else {
-      response.json(results);
-    }
-  });
+   const query = "SELECT * FROM songs ORDER BY songName";
+   connection.query(query, (error, results, fields) => {
+      if (error) {
+         console.log(error);
+      } else {
+         response.json(results);
+      }
+   });
 });
